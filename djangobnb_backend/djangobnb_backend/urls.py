bb15bc041dd6791b94fd6cac96dc4e53d64f7e4d
Stein Ove Helset <steinovehelset@gmail.com>
2024-03-03 18:55:15 +0000

    part 4

--- djangobnb_backend/djangobnb_backend/urls.py
+++ djangobnb_backend/djangobnb_backend/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/properties/', include('property.urls')),
+    path('api/auth/', include('useraccount.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ djangobnb_backend/useraccount/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from dj_rest_auth.jwt_auth import get_refresh_view
+from dj_rest_auth.registration.views import RegisterView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
+from rest_framework_simplejwt.views import TokenVerifyView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='rest_register'),
+    path('login/', LoginView.as_view(), name='rest_login'),
+    path('logout/', LogoutView.as_view(), name='rest_logout'),
+]
