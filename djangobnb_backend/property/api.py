455d2f38b6914e30f6c2a74463bbb4b7db270f3b
Code With Stein <codewithstein@gmail.com>
2024-03-31 20:21:43 +0200

    part 7

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -34,11 +34,46 @@ def properties_list(request):
     is_favorites = request.GET.get('is_favorites', '')
     landlord_id = request.GET.get('landlord_id', '')
 
+    country = request.GET.get('country', '')
+    category = request.GET.get('category', '')
+    checkin_date = request.GET.get('checkIn', '')
+    checkout_date = request.GET.get('checkOut', '')
+    bedrooms = request.GET.get('numBedrooms', '')
+    guests = request.GET.get('numGuests', '')
+    bathrooms = request.GET.get('numBathrooms', '')
+
+    print('country', country)
+
+    if checkin_date and checkout_date:
+        exact_matches = Reservation.objects.filter(start_date=checkin_date) | Reservation.objects.filter(end_date=checkout_date)
+        overlap_matches = Reservation.objects.filter(start_date__lte=checkout_date, end_date__gte=checkin_date)
+        all_matches = []
+
+        for reservation in exact_matches | overlap_matches:
+            all_matches.append(reservation.property_id)
+        
+        properties = properties.exclude(id__in=all_matches)
+
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
 
     if is_favorites:
         properties = properties.filter(favorited__in=[user])
+    
+    if guests:
+        properties = properties.filter(guests__gte=guests)
+    
+    if bedrooms:
+        properties = properties.filter(bedrooms__gte=bedrooms)
+    
+    if bathrooms:
+        properties = properties.filter(bathrooms__gte=bathrooms)
+    
+    if country:
+        properties = properties.filter(country=country)
+    
+    if category and category != 'undefined':
+        properties = properties.filter(category=category)
     
     #
     # Favorites
--- djangobnb_backend/useraccount/urls.py
+++ djangobnb_backend/useraccount/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='rest_register'),
     path('login/', LoginView.as_view(), name='rest_login'),
     path('logout/', LogoutView.as_view(), name='rest_logout'),
+    path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
     path('myreservations/', api.reservations_list, name='api_reservations_list'),
     path('<uuid:pk>/', api.landlord_detail, name='api_landlrod_detail'),
 ]
