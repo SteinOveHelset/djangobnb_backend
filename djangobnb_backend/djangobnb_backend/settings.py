bde4f9fe4becaa2133d7aff490e27e71fc510407
Code With Stein <codewithstein@gmail.com>
2024-04-07 20:36:19 +0200

    added dotenv

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["64.226.81.32"]
 
 AUTH_USER_MODEL = 'useraccount.User'
 
