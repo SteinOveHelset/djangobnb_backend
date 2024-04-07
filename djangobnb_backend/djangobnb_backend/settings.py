44637c2c54004b1c6b4195737d5486d712640237
Stein Ove Helset <steinovehelset@gmail.com>
2024-04-07 19:32:31 +0200

    server files

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -22,7 +22,10 @@ AUTH_USER_MODEL = 'useraccount.User'
 
 SITE_ID = 1
 
-WEBSITE_URL = 'http://localhost:8000'
+if DEBUG:
+    WEBSITE_URL = 'http://localhost:8000'
+else:
+    WEBSITE_URL = 'http://64.226.81.32:1337'
 
 CHANNEL_LAYERS = {
     'default': {
@@ -58,6 +61,22 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
+    'http://64.226.81.32',
+    'http://64.226.81.32:1337'
+]
+
+CORS_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://64.226.81.32',
+    'http://64.226.81.32:1337'
+]
+
+CORS_ORIGINS_WHITELIST = [
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:3000',
+    'http://64.226.81.32',
+    'http://64.226.81.32:1337'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
