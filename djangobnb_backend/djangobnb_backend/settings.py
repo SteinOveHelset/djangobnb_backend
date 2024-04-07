b07ff25d99d60c169df77ab10fd8a5f500cb08d3
Code With Stein <codewithstein@gmail.com>
2024-04-07 20:34:14 +0200

    added dotenv

--- djangobnb_backend/djangobnb_backend/settings.py
+++ djangobnb_backend/djangobnb_backend/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://64.226.81.32:1337'
 ]
 
-CORS_TRUSTED_ORIGINS = [
+CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://127.0.0.1:3000',
     'http://64.226.81.32',
